d02e915ecbedcb05a715775c54a01aa272c1a9e7
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-03-05 02:40:55 +0100

    Add react-router-dom Links to fix production build issue

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 const NavigationSidebar = (
     {
         active = 'explore'
@@ -6,38 +8,49 @@ const NavigationSidebar = (
 ) => {
     return (
     <div className="list-group">
-        <a className="list-group-item">Tuiter</a>
-        <a className={`list-group-item
-                        ${active === 'home'?'active':''}`}>
-            Home
-        </a>
-        <a className={`list-group-item
-                        ${active === 'explore'?'active':''}`}>
-            Explore
-        </a>
-        <a className={`list-group-item
-                        ${active === 'notifications'?'active':''}`}>
-            Notifications
-        </a>
-        <a className={`list-group-item
-                        ${active === 'messages'?'active':''}`}>
-            Messages </a>
-        <a className={`list-group-item
-                        ${active === 'bookmarks'?'active':''}`}>
-            Bookmarks
-        </a>
-        <a className={`list-group-item
-                        ${active === 'lists'?'active':''}`}>
-            Lists
-        </a>
-        <a className={`list-group-item
-                        ${active === 'profile'?'active':''}`}>
-            Profile
-        </a>
-        <a className={`list-group-item
-                        ${active === 'more'?'active':''}`}>
-            More
-        </a>
+        <Link to="/tuiter" className="list-group-item list-group-item-action">
+            <i className="bi bi-twitter"></i>
+        </Link>
+        <Link to="/tuiter/home" className={`list-group-item list-group-item-action
+                    ${active === 'home'?'active':''}`}>
+            <i className="bi bi-house-door-fill"></i>
+            <span className="d-none d-xl-inline">Home</span>
+        </Link>
+        <Link to="/tuiter/explore" className={`list-group-item list-group-item-action
+                    ${active === 'explore'?'active':''}`}>
+            <i className="bi bi-hash"></i>
+            <span className="d-none d-xl-inline">Explore</span>
+        </Link>
+        <Link to="/tuiter/notifications" className={`list-group-item list-group-item-action
+                    ${active === 'notifications'?'active':''}`}>
+            <i className="bi bi-bell-fill"></i>
+            <span className="d-none d-xl-inline">Notifications</span>
+        </Link>
+        <Link to="/tuiter/messages" className={`list-group-item list-group-item-action
+                    ${active === 'messages'?'active':''}`}>
+            <i className="bi bi-envelope-fill"></i>
+            <span className="d-none d-xl-inline">Messages</span>
+        </Link>
+        <Link to="/tuiter/bookmarks" className={`list-group-item list-group-item-action
+                    ${active === 'bookmarks'?'active':''}`}>
+            <i className="bi bi-bookmark-fill"></i>
+            <span className="d-none d-xl-inline">Bookmarks</span>
+        </Link>
+        <Link to="/tuiter/lists" className={`list-group-item list-group-item-action
+                    ${active === 'lists'?'active':''}`}>
+            <i className="bi bi-list"></i>
+            <span className="d-none d-xl-inline">Lists</span>
+        </Link>
+        <Link to="/tuiter/profile" className={`list-group-item list-group-item-action
+                    ${active === 'profile'?'active':''}`}>
+            <i className="bi bi-person-fill"></i>
+            <span className="d-none d-xl-inline">Profile</span>
+        </Link>
+        <Link to="/tuiter/more" className={`list-group-item list-group-item-action
+                    ${active === 'more'?'active':''}`}>
+            <i className="bi bi-three-dots"></i>
+            <span className="d-none d-xl-inline">More</span>
+        </Link>
     </div>
     );
 };
