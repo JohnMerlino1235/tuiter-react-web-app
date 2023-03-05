4405304e9ba566a3a2777f4399d3cf00fd002a75
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-03-05 02:24:47 +0100

    A6 Implementation

---
+++ src/labs/a6/index.js
@@ -0,0 +1,14 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
+function Assignment6() { return (
+    <div>
+        <h1>Assignment 6</h1>
+        <TodoList></TodoList>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+    </div> );
+}
+export default Assignment6;
