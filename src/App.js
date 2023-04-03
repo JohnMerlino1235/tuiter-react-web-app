76071bb725cdf3bb5a53c158f730c820cc9849c7
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-02 23:35:57 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  const TUITS_API = `${API_BASE}/tuits`;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Tuits: {TUITS_API}
         </p>
         <a
           className="App-link"
